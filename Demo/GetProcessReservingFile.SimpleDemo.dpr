887732fc5b4bdb8785f46a3cf9aefacb852cdb53
Tommi Prami <tommi.prami@gmail.com>
2023-03-15 10:16:37 +0200

    Fixed project file

--- Demo/GetProcessReservingFile.SimpleDemo.dpr
+++ Demo/GetProcessReservingFile.SimpleDemo.dpr
@@ -2,6 +2,8 @@
 
 uses
   Vcl.Forms,
+  UnitGetProcessReservingFile.Types in'..\Source\UnitGetProcessReservingFile.Types.pas',
+  UnitGetProcessReservingFile in '..\Source\UnitGetProcessReservingFile.pas',
   FormGetProcessReservingFileSimpleDemo in 'FormGetProcessReservingFileSimpleDemo.pas' {GetProcessReservingFileSimpleDemoForm};
 
 {$R *.res}
@@ -9,6 +11,6 @@ uses
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm8, Form8);
+  Application.CreateForm(TGetProcessReservingFileSimpleDemoForm, GetProcessReservingFileSimpleDemoForm);
   Application.Run;
 end.
