96bed7a875828627f37586e3a2136d8680eae90e
Tommi Prami <tommi.prami@gmail.com>
2023-03-16 13:49:19 +0200

    Renamed variables and small refactoring

--- Source/UnitGetProcessReservingFile.pas
+++ Source/UnitGetProcessReservingFile.pas
@@ -9,65 +9,76 @@ implementation
 uses
   Winapi.Windows, System.SysUtils, UnitGetProcessReservingFile.Types;
 
+function CheckProcessTime(const AProcessHandle: THandle; const AProcessStartTime: FILETIME): Boolean;
+var
+  LCreateTime: TFileTime;
+  LExitTile: TFileTime;
+  LKernelTime: TFileTime;
+  LUserTime: TFileTime;
+begin
+  Result := GetProcessTimes(AProcessHandle, LCreateTime, LExitTile, LKernelTime, LUserTime);
+
+  if Result then
+    Result := CompareFileTime(@AProcessStartTime, @LCreateTime) = 0;
+end;
+
 function GetProcessReservingFile(const AFileName: string; out AProcessName: string): Boolean;
 var
-  dwSession: DWORD;
-  szSessionKey: array[0..CCH_RM_MAX_SESSION_KEY] of Char;
-  dwError: DWORD;
-  pszFile: PCWSTR;
-  dwReason: DWORD;
+  LSessionHandle: DWORD;
+  LSessionKeyBuffer: array[0..CCH_RM_MAX_SESSION_KEY] of Char;
+  LApiResult: DWORD;
+  LFilenamePointer: PCWSTR;
+  LReason: DWORD;
   LIndex: Integer;
-  nProcInfoNeeded: UINT;
-  nProcInfo: UINT;
-  rgpi: array[0..9] of RM_PROCESS_INFO;
-  hProcess: THandle;
-  ftCreate, ftExit, ftKernel, ftUser: TFileTime;
-  sz: array[0..MAX_PATH - 1] of Char;
-  cch: Cardinal;
+  LProcInfoNeeded: UINT;
+  LProcInfoCount: UINT;
+  LProcessInfoArray: array[0..9] of RM_PROCESS_INFO;
+  LProcessHandle: THandle;
+  LProcessNameBuffer: array[0..MAX_PATH - 1] of Char;
+  LBufferSize: Cardinal;
 begin
   Result := False;
 
-  FillChar(szSessionKey, SizeOf(szSessionKey), #0);
-  dwError := RmStartSession(dwSession, 0, szSessionKey);
+  FillChar(LSessionKeyBuffer, SizeOf(LSessionKeyBuffer), #0);
+  LApiResult := RmStartSession(LSessionHandle, 0, LSessionKeyBuffer);
 
-  if (dwError = ERROR_SUCCESS) then
+  if (LApiResult = ERROR_SUCCESS) then
   try
-    pszFile := PCWSTR(AFileName);
-    dwError := RmRegisterResources(dwSession, 1, @pszFile, 0, nil, 0, nil);
+    LFilenamePointer := PCWSTR(AFileName);
+    LApiResult := RmRegisterResources(LSessionHandle, 1, @LFilenamePointer, 0, nil, 0, nil);
 
-    if dwError = ERROR_SUCCESS then
+    if LApiResult = ERROR_SUCCESS then
     begin
-      nProcInfo := SizeOf(rgpi) div SizeOf(RM_PROCESS_INFO);
+      LProcInfoCount := SizeOf(LProcessInfoArray) div SizeOf(RM_PROCESS_INFO);
 
-      dwError := RmGetList(dwSession, nProcInfoNeeded, nProcInfo, @rgpi[0], dwReason);
+      LApiResult := RmGetList(LSessionHandle, LProcInfoNeeded, LProcInfoCount, @LProcessInfoArray[0], LReason);
 
-      if dwError = ERROR_SUCCESS then
+      if LApiResult = ERROR_SUCCESS then
       begin
-        for LIndex := 0 to nProcInfo - 1 do
+        for LIndex := 0 to LProcInfoCount - 1 do
         begin
-          hProcess := OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, False, rgpi[LIndex].Process.dwProcessId);
+          LProcessHandle := OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, False, LProcessInfoArray[LIndex].Process.dwProcessId);
 
-          if hProcess <> 0 then
+          if LProcessHandle <> 0 then
           try
-            if GetProcessTimes(hProcess, ftCreate, ftExit, ftKernel, ftUser)
-              and (CompareFileTime(@rgpi[LIndex].Process.ProcessStartTime, @ftCreate) = 0) then
+            if CheckProcessTime(LProcessHandle, LProcessInfoArray[LIndex].Process.ProcessStartTime) then
             begin
-              cch := MAX_PATH;
+              LBufferSize := MAX_PATH;
 
-              if QueryFullProcessImageNameW(hProcess, 0, sz, cch) and (cch <= MAX_PATH) then
+              if QueryFullProcessImageNameW(LProcessHandle, 0, LProcessNameBuffer, LBufferSize) and (LBufferSize <= MAX_PATH) then
               begin
-                AProcessName := sz;
+                AProcessName := LProcessNameBuffer;
                 Exit(True);
               end;
             end;
           finally
-            CloseHandle(hProcess);
+            CloseHandle(LProcessHandle);
           end;
         end;
       end;
     end;
   finally
-    RmEndSession(dwSession);
+    RmEndSession(LSessionHandle);
   end;
 end;
 
